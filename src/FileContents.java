44583a2ec3dd35c53f7b96184ff90fc633eae0bb
eddrickliu <eddrickliu@yahoo.com>
2019-06-07 15:29:22 -0700

    first commit

---
+++ src/FileContents.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class FileContents implements Serializable {
+    private byte[] contents; // file contents
+    public FileContents( byte[] contents ) {
+        this.contents = contents;
+    }
+    public void print( ) throws IOException {
+        System.out.println( "FileContents = " + contents );
+    }
+    public byte[] get( ) {
+        return contents;
+    }
+}
